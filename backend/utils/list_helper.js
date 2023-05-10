cd20996fbdc610c7c56fae31cd2af9f287b697f4
ktatu <kaikkonentatu@gmail.com>
2023-05-11 02:06:10 +0300

    Update list_helper.js

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,49 +1,53 @@
 const _ = require("lodash")
 
+//eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
-	return 1
+    return 1
 }
 
-const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
-const favoriteBlog = blogs => {
-	let blogWithMostLikes = { likes : -1 }
-	for (let blog of blogs) {
-		blogWithMostLikes = blog.likes < blogWithMostLikes.likes ? blogWithMostLikes : blog
-	}
-	return blogWithMostLikes.likes === -1 ? undefined : blogWithMostLikes
+const favoriteBlog = (blogs) => {
+    let blogWithMostLikes = { likes: -1 }
+    for (let blog of blogs) {
+        blogWithMostLikes = blog.likes < blogWithMostLikes.likes ? blogWithMostLikes : blog
+    }
+    return blogWithMostLikes.likes === -1 ? undefined : blogWithMostLikes
 }
 
-const mostBlogs = blogs => {
-	let authorsAndBlogCounts = _.map(_.countBy(blogs, "author"), (val, key) => ({ author: key, blogs: val}))
+const mostBlogs = (blogs) => {
+    let authorsAndBlogCounts = _.map(_.countBy(blogs, "author"), (val, key) => ({
+        author: key,
+        blogs: val,
+    }))
 
-	let authorWithMostBlogs = _.head(_.orderBy(authorsAndBlogCounts, ["blogs"], ["desc"]))
+    let authorWithMostBlogs = _.head(_.orderBy(authorsAndBlogCounts, ["blogs"], ["desc"]))
 
-	return authorWithMostBlogs
+    return authorWithMostBlogs
 }
 
-const mostLikes = blogs => {
-	let authorAndLikesMapped = _.map(blogs, (blog) => {
-		return { author: blog.author, likes: blog.likes}
-	})
+const mostLikes = (blogs) => {
+    let authorAndLikesMapped = _.map(blogs, (blog) => {
+        return { author: blog.author, likes: blog.likes }
+    })
 
-	let authorsReducedByLikes = authorAndLikesMapped.reduce((array, entry) => {
-		let entryInArrayIndex = _.findIndex(array, { "author": entry.author })
-		if (entryInArrayIndex !== -1) {
-			array[entryInArrayIndex].likes += entry.likes
-		} else {
-			array = array.concat(entry)
-		}
-		return array
-	}, [])
+    let authorsReducedByLikes = authorAndLikesMapped.reduce((array, entry) => {
+        let entryInArrayIndex = _.findIndex(array, { author: entry.author })
+        if (entryInArrayIndex !== -1) {
+            array[entryInArrayIndex].likes += entry.likes
+        } else {
+            array = array.concat(entry)
+        }
+        return array
+    }, [])
 
-	return _.last(_.sortBy(authorsReducedByLikes, "likes"))
+    return _.last(_.sortBy(authorsReducedByLikes, "likes"))
 }
 
 module.exports = {
-	dummy,
-	totalLikes,
-	favoriteBlog,
-	mostBlogs,
-	mostLikes
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes,
 }
