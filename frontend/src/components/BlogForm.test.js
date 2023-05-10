5238b302533de6fbc2a3e61c878269a6ba6f1c12
ktatu <kaikkonentatu@gmail.com>
2023-05-11 02:31:11 +0300

    Lint fixes

--- frontend/src/components/BlogForm.test.js
+++ frontend/src/components/BlogForm.test.js
@@ -1,17 +1,16 @@
 import BlogForm from "./BlogForm"
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 const blogInputValues = {
     author: "Test Author",
     title: "Test Title",
-    url: "https://example.com"
+    url: "https://example.com",
 }
 
 const mockCreateBlog = jest.fn()
 
 test("createBlog gets called with correct blog values", async () => {
-
     const container = render(<BlogForm createBlog={mockCreateBlog} />).container
     const submitBlogButton = container.querySelector("#blog-submit")
 
