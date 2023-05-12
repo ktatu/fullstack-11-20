d15fbda62ddbbef0d575b60cf926042abf2f6786
ktatu <kaikkonentatu@gmail.com>
2023-05-12 16:33:15 +0300

    Workflow test

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,13 +1,13 @@
 require("dotenv").config()
 
-let PORT = process.env.PORT
+let PORT = 3003
 let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === "test") {
-	MONGODB_URI = process.env.TEST_MONGODB_URI
+    MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
 module.exports = {
-	PORT,
-	MONGODB_URI
+    PORT,
+    MONGODB_URI,
 }
